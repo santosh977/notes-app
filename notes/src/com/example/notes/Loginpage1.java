2d31fbe6989e22b29abca31730c3dc64e609dd78
Divyavrat <divyavrat@gmail.com>
2014-07-09 01:50:39 +0530

    Fixed User Database (beta).

--- notes/src/com/example/notes/Loginpage1.java
+++ notes/src/com/example/notes/Loginpage1.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class Loginpage1 extends Activity {
@@ -20,27 +21,27 @@ public class Loginpage1 extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.loginpage1);}
 	public void Function8(View v) {
+		boolean founduser=false;
 		DatabaseHandler db = new DatabaseHandler(this);
 		List<Users> users = db.getAllContacts();       
-        
+		//Toast.makeText(getApplicationContext(),(((EditText)findViewById(R.id.Editpass)).getText()).toString(), Toast.LENGTH_LONG).show();
         for (Users cn : users) {
             //String log = "Id: "+cn.getID()+" ,Name: " + cn.getName() + " ,Phone: " + cn.getPhoneNumber();
                 // Writing Contacts to log
         //Log.d("Name: ", log);
-         if( cn.getPassword()==((EditText)findViewById(R.id.Editpass)).toString()&&cn.getPassword()==((EditText)findViewById(R.id.Editpass)).toString())
-         {
-        	 startActivity(new Intent(Loginpage1.this,Homepage1.class)); 
-         }
-        
-         else
-         {
-        	  Toast.makeText(getApplicationContext(),"Wrong Email or Password", Toast.LENGTH_LONG).show();
-        	 }
-        }
-		
-		
-		
-	     }	
+         if( cn.getPassword().equals((((EditText)findViewById(R.id.Editpass)).getText()).toString())&&cn.getEmail().equals((((EditText)findViewById(R.id.EditMail)).getText()).toString()))
+         {founduser=true;
+        	 startActivity(new Intent(Loginpage1.this,Homepage1.class));
+        	 finish();
+         }}
+        /*TextView tv=(TextView)findViewById(R.id.textView1);
+        tv.setText("UsersList:");
+        //DatabaseHandler db = new DatabaseHandler(this);
+		//List<Users> users1 = db.getAllContacts();       
+        for (Users cn : users) {
+        tv.setText(tv.getText()+":"+cn.getName());}*/
+if(!founduser)Toast.makeText(getApplicationContext(),"Wrong Email or Password", Toast.LENGTH_LONG).show();
+        }	
 	public void Function9(View v) {
 		startActivity(new Intent(Loginpage1.this,Signup.class));
 		
--- notes/src/com/example/notes/Signup.java
+++ notes/src/com/example/notes/Signup.java
@@ -1,12 +1,10 @@
 package com.example.notes;
 
-import java.util.List;
-
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 public class Signup extends Activity {
@@ -19,7 +17,7 @@ public class Signup extends Activity {
 		public void Function2(View v) {
 			//Toast.makeText(getApplicationContext(),((EditText)findViewById(R.id.editText9)).getText().toString(), Toast.LENGTH_LONG).show();
 			
-			if(((EditText)findViewById(R.id.editText9)).getText().toString()==((EditText)findViewById(R.id.editText10)).getText().toString()||true)
+			if(((EditText)findViewById(R.id.editText9)).getText().toString().equals(((EditText)findViewById(R.id.editText10)).getText().toString()))
 			{
 			DatabaseHandler db = new DatabaseHandler(this);
 	        db.addContact(new Users(((EditText)findViewById(R.id.editText1)).getText().toString(),
@@ -30,16 +28,9 @@ public class Signup extends Activity {
 	        ,((EditText)findViewById(R.id.editText5)).getText().toString()
 	        ,((EditText)findViewById(R.id.editText2)).getText().toString()
 	        ,((EditText)findViewById(R.id.editText9)).getText().toString()));
-			//startActivity(new Intent(Signup.this,Homepage1.class));
-			//finish();
-	        TextView tv=(TextView)findViewById(R.id.textView1);
-	        tv.setText("UsersList::");
-	        //DatabaseHandler db = new DatabaseHandler(this);
-			List<Users> users = db.getAllContacts();       
-	        
-	        for (Users cn : users) {
-	        tv.setText(tv.getText()+"\n"+cn.getName());}
-			}
+			startActivity(new Intent(Signup.this,Homepage1.class));
+			finish();
+	        }
 			else
 	         {
 	        	  Toast.makeText(getApplicationContext(),"Confirm and the normal one are not twins.", Toast.LENGTH_LONG).show();
